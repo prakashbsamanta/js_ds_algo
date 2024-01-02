ba11934882a003e8f117d293dcf2b476fdae9f39
prakashbsamanta <prakashbsamanta@gmail.com>
2024-01-02 23:16:07 +0530

    fix: 169. MajorityElement with moore's Algo

--- leetyard_Solutions/169.MajorityElement.js
+++ leetyard_Solutions/169.MajorityElement.js
@@ -16,10 +16,49 @@ Input: nums = [2,2,1,1,1,2,2]
 Output: 2
  */
 
-var majorityElement = function (nums) {
-  let sortedNums = nums.sort();
-  return sortedNums[Math.floor(nums.length / 2)];
-};
+// var majorityElement = function (nums) {
+//   let sortedNums = nums.sort();
+//   return sortedNums[Math.floor(nums.length / 2)];
+// };
 
-let nums = [1, 2, 3, 4, 4, 4, 5, 5, 2, 2, 2, 2, 2];
+//  Alternate solution:
+// var majorityElement = function (nums) {
+//   let maxLen = Math.floor(nums.length / 2);
+//   console.log(maxLen);
+//   let elementFrequency = {};
+//   for (let num of nums) {
+//     if (elementFrequency.hasOwnProperty(num)) {
+//       elementFrequency[num]++;
+//     } else {
+//       elementFrequency[num] = 1;
+//     }
+//   }
+//   for (let num in elementFrequency) {
+//     if (elementFrequency[num] >= maxLen) {
+//       console.log();
+//       return num;
+//     }
+//   }
+//   return null;
+// };
+
+// Moore's voting Algorithm
+
+function majorityElement(nums) {
+  let count = 0;
+  let result;
+  for (let i = 0; i < nums.length; i++) {
+    if (count === 0) {
+      result = nums[i];
+    }
+    if (nums[i] === result) {
+      count++;
+    } else {
+      count--;
+    }
+  }
+  return result;
+}
+
+let nums = [1, 2, 3, 4, 2, 2, 4, 2, 2];
 console.log(majorityElement(nums));
